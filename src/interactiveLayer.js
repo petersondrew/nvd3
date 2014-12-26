eddc1a88565f923eff3a8c335fac73ac7e354cd8
Drew Peterson <dpeterson@roadrangerusa.com>
2014-12-26 10:30:46 -0600

    Fix exception when mousing over dev tools in IE

--- src/interactiveLayer.js
+++ src/interactiveLayer.js
@@ -97,7 +97,8 @@ nv.interactiveGuideline = function() {
                     if (isMSIE) {
                         if (d3.event.relatedTarget
                             && d3.event.relatedTarget.ownerSVGElement === undefined
-                            && d3.event.relatedTarget.className.match(tooltip.nvPointerEventsClass)) {
+                            && (d3.event.relatedTarget.className === undefined
+                                || d3.event.relatedTarget.className.match(tooltip.nvPointerEventsClass))) {
 
                             return;
                         }
